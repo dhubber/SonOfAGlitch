8b9503904b6a2aae247ee0daa4735737917a817c
Sebastian Parfitt <sebastian@shytea.games>
2020-12-12 12:42:46 +0100

    Refactored glitch base and glitch area, so all glitches can now have an active time

--- Scenes/Seb/Glitch System/Bases/Glitch Area Base.gd
+++ Scenes/Seb/Glitch System/Bases/Glitch Area Base.gd
@@ -1,44 +1,25 @@
-extends "res://Scenes/Seb/Glitch System/Glitch Base.gd"
+extends "res://Scenes/Seb/Glitch System/Bases/Glitch Base.gd"
 
-export var activeTime : float = 2
-var glitchActive : bool
+
 var affectedRunners = []
 var inrangeRunners = []
-
-	
-func _process(delta):
-	._process(delta)
-
-#	Deactivate if time passed since activation is larger than active time
-	if glitchActive && cooldownTimeRemaining < cooldownTime - activeTime:
-		DeactivateGlitch()
-		
 
 func OnActivateGlitch():
 	if glitchActive: return
 	.OnActivateGlitch()
-	text.modulate.r = 1
-	text.modulate.g = 0
-	text.modulate.b = 0
 
 #	Glitch all runners in range
 	for r in inrangeRunners:
 		GlitchRunner(r)
-		
-	glitchActive = true
 
 
-func DeactivateGlitch():
+func OnDeactivateGlitch():
 	if !glitchActive: return
-	text.modulate.r = 0
-	text.modulate.g = 0
-	text.modulate.b = 1
+	.OnDeactivateGlitch()
 	
-	#	Ungitch all runners in range
+	#	Unglitch all runners in range
 	for r in affectedRunners:
 		UnglitchRunner(r)
-		
-	glitchActive = false
 
 
 func OnRunnerEnter(body):
@@ -56,9 +37,11 @@ func OnRunnerExit(body):
 
 
 func GlitchRunner(runner):
+	print("Glitching " + runner.name)
 	affectedRunners.push_back(runner)
 	return
 
 func UnglitchRunner(runner):
+	print("Unglitching " + runner.name)
 	affectedRunners.erase(runner)
 	return
