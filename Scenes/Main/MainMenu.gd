221dc56e184ad8ad6b56ef9b386fdd2d7478780e
Viktoria Kirchleitner <34382803+VeeChurchill@users.noreply.github.com>
2020-12-12 22:16:10 +0100

    Buttons in MainMenu work pt1

--- Scenes/Main/MainMenu.gd
+++ Scenes/Main/MainMenu.gd
@@ -1,16 +1,35 @@
 extends MarginContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+var start_button
+var exit_button
+var credits_button
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	start_button = get_node("HSplitContainer/VBoxContainer/MenuOptions/Start")
+	exit_button = get_node("HSplitContainer/VBoxContainer/MenuOptions/Exit")
+	credits_button = get_node("HSplitContainer/VBoxContainer/MenuOptions/Credits")
+	
+	
+	start_button.connect("pressed", self, "_start_pressed")
+	exit_button.connect("pressed", self, "_exit_pressed")
+	credits_button.connect("pressed", self, "_credits_pressed")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _start_pressed():
+
+	get_tree().change_scene("res://Scenes/Main/Main_Scene.tscn")
+
+func _exit_pressed():
+	get_tree().quit()
+
+func _credits_pressed():
+	print("Credits pressed")
