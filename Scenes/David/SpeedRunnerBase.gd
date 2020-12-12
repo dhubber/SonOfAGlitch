d54f68d429efce91d4cb59ca92351c9d938d35c0
David Hubber <david.hubber@gmail.com>
2020-12-12 13:18:25 +0100

    SpeedRunner animations plus AI test map

--- Scenes/David/SpeedRunnerBase.gd
+++ Scenes/David/SpeedRunnerBase.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
-
 export var walk_speed = 200.0 
+
+onready var animatedSprite = get_node("AnimatedSprite")
+
+var direction = Vector2()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,9 +12,18 @@ func _ready():
 	pass # Replace with function body.
 
 
-func move_direction(direction):
-	var destination = global_position + 100 * direction
+func move_direction():
 	var velocity = direction * walk_speed
-#	print("velocity : ", velocity)
-	look_at(destination)
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
+	direction = velocity.normalized()
+
+
+func animate():
+	if direction.length_squared() > 0:
+		animatedSprite.play("walk")
+		if direction.x > 0:
+			animatedSprite.flip_h = true
+		elif direction.x < 0:
+			animatedSprite.flip_h = false
+	else:
+		animatedSprite.play("idle")
