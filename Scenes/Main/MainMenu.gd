cd21469cd6f05be5b741519e90b572de91d46e67
Viktoria Kirchleitner <34382803+VeeChurchill@users.noreply.github.com>
2020-12-13 12:24:32 +0100

    Lol something like a menu screen i guess

--- Scenes/Main/MainMenu.gd
+++ Scenes/Main/MainMenu.gd
@@ -5,19 +5,23 @@ extends MarginContainer
 var start_button
 var exit_button
 var credits_button
+var about_button
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_button = get_node("HSplitContainer/VBoxContainer/MenuOptions/Start")
-	exit_button = get_node("HSplitContainer/VBoxContainer/MenuOptions/Exit")
-	credits_button = get_node("HSplitContainer/VBoxContainer/MenuOptions/Credits")
+	start_button = get_node("HSplitContainer/CenterContainer2/MenuOptions/Start")
+	exit_button = get_node("HSplitContainer/CenterContainer2/MenuOptions/Exit")
+	credits_button = get_node("HSplitContainer/CenterContainer2/MenuOptions/Credits")
+	about_button = get_node("HSplitContainer/CenterContainer2/MenuOptions/About")
+	
 	
 	
 	start_button.connect("pressed", self, "_start_pressed")
 	exit_button.connect("pressed", self, "_exit_pressed")
 	credits_button.connect("pressed", self, "_credits_pressed")
+	about_button.connect("pressed", self, "_about_pressed")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,4 +36,7 @@ func _exit_pressed():
 	get_tree().quit()
 
 func _credits_pressed():
+	print("Credits pressed")
+	
+func _about_pressed():
 	print("Credits pressed")
